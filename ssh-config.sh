dec984942d4aeb832f44db7119d43ca2df25164d
greyspectrum <greyspectrum@users.noreply.github.com>
2016-06-24 22:31:29 +0000

    typo

--- ssh-config.sh
+++ ssh-config.sh
@@ -17,7 +17,7 @@
 # Verify that script is running as root
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
+   echo "This script must be run as root." 1>&2
    exit 1
 fi
 
