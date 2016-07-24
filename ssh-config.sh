759d161e261c0cd476a7ebcd1b8e40fce60107c0
greyspectrum <greyspectrum@users.noreply.github.com>
2016-07-24 22:18:48 +0000

    Update ssh-config.sh

--- ssh-config.sh
+++ ssh-config.sh
@@ -10,8 +10,8 @@
 # https://stribika.github.io/2015/01/04/secure-secure-shell.html
 #
 # :author: greyspectrum
-# :date: 20 July 2016
-# :version: 0.0.4
+# :date: 24 July 2016
+# :version: 0.0.5
 ##############################################################################
 
 # Define variables (edit these if you want to test this script without altering your ssh_config)
@@ -30,19 +30,6 @@ ALL_MODULI="/etc/ssh/moduli.all"
 
 SAFE_MODULI="/etc/ssh/moduli.safe"
 
-# Introduce changes to the user and ask for confirmation before running
-
-echo -e "This script will apply the following changes to ssh_config:\n\nKey Exchange Algorithms:\n-ECDH over Curve25519 with SHA2\n-Diffie Hellman Group Exchange with SHA2\n\nServer Authentication:\n-Ed25519 with SHA512\n-RSA with SHA1\n\nClient Authentication:\n-Password Authentication: no\n-Challenge Response Authentication: no\n-Public Key Authentication: yes\n\nHost Keys will be created, after an additional prompt, with Ed25519 and 4096 RSA. If you already have host keys, you may skip this step.\n\nSymmetric Ciphers & Modes of Operation:\n-Chacha20\n-AES 256, GCM\n-AES 128, GCM\n-AES 256, CTR\n-AES 192, CTR\n-AES 128, CTR\n\nMessage Authentication Codes:\n-hmac-sha2-512-etm\n-hmac-sha2-256-etm\n-hmac-ripemd160-etm\n-umac-128-etm\n-hmac-sha2-512\n-hmac-sha2-256\n-hmac-ripemd160\n-umac-128\n\nHashKnownHosts: yes\n\nUse Roaming: no\n\nInsecure options will be disabled. Your current ssh_config will be backed up at /etc/ssh/ssh_config.bak.\n"
-
-while true; do
-    read -p "Do you wish to apply these changes?" yn
-    case $yn in
-        [Yy]* ) echo -e "\nStarting ssh_config...\n"; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer y (yes) or n (no).";;
-    esac
-done
-
 # Prompt the user for root
 
 [ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
@@ -54,6 +41,73 @@ cp $SSH_CONFIG $BACKUP_SSH_CONFIG
 # Create new ssh config file
 
 echo -e "    PasswordAuthentication no \n    ChallengeResponseAuthentication no \n    Host * \n          PasswordAuthentication no \n          ChallengeResponseAuthentication no \n    PubkeyAuthentication yes \n    Host * \n           PubkeyAuthentication yes \n           HostAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa \n    KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256 \n#   Github needs diffie-hellman-group-exchange-sha1 some of the time but not always. \n    Host github.com \n           KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256,diffie-hellman-group-exchange-sha1,diffie-hellman-group14-sha1 \n    Host * \n           KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256 \n    Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr \n    Host * \n           Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr \n    MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-ripemd160-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,hmac-ripemd160,umac-128@openssh.com \n    Host * \n           MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-ripemd160-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,hmac-ripemd160,umac-128@openssh.com \n    HashKnownHosts yes \n    UseRoaming no \n    Host * \n           UseRoaming no" > $SECURE_SSH_CONFIG
+
+# Examine existing ssh_config and promt user to select changes
+
+if grep -Fxq "PasswordAuthentication yes" $SSH_CONFIG ; then
+        while true; do
+                read -p "Password Authentication is set to \"yes\". Change to \"no\" (strongly recommended)?" yn
+                case $yn in
+                        [Yy]* ) exit ;;
+                        [Nn]* ) sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' $SECURE_SSH_CONFIG ;;
+                        * ) echo "Please answer y (yes) or n (no).";;
+                esac
+        done
+else echo "Password Authentication is set to \"no\". This is the recommended setting."
+
+fi
+
+if grep -Fxq "ChallengeResponseAuthentication yes" $SSH_CONFIG ; then
+        while true; do
+                read -p "Challenge Response Authentication is set to \"yes\". Change to \"no\" (strongly recommended)?" yn
+                case $yn in
+                        [Yy]* ) exit ;;
+                        [Nn]* ) sed -i 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/g' $SECURE_SSH_CONFIG ;;
+                        * ) echo "Please answer y (yes) or n (no).";;
+                esac
+        done
+else echo "Challenge Response Authentication is set to \"no\". This is the recommended setting."
+
+fi
+
+if grep -Fxq "PubkeyAuthentication no" $SSH_CONFIG ; then
+        while true; do
+                read -p "Pubkey Authentication is set to \"no\". Change to \"yes\" (strongly recommended)?" yn
+                case $yn in
+                        [Yy]* ) exit ;;
+                        [Nn]* ) sed -i 's/PubkeyAuthentication yes/ChallengeResponseAuthentication no/g' $SECURE_SSH_CONFIG ;;
+                        * ) echo "Please answer y (yes) or n (no).";;
+                esac
+        done
+else echo "Pubkey Authentication is set to \"yes\". This is the recommended setting."
+
+fi
+
+if grep -Fxq "HashKnownHosts no" $SSH_CONFIG ; then
+        while true; do
+                read -p "Hash Known Hosts is set to \"no\". Change to \"yes\" (strongly recommended)?" yn
+                case $yn in
+                        [Yy]* ) exit ;;
+                        [Nn]* ) sed -i 's/HashKnownHosts yes/HashKnownHosts no/g' $SECURE_SSH_CONFIG ;;
+                        * ) echo "Please answer y (yes) or n (no).";;
+                esac
+        done
+else echo "Hash Known Hosts is set to \"yes\". This is the recommended setting."
+
+fi
+
+if grep -Fxq "UseRoaming yes" $SSH_CONFIG ; then
+        while true; do
+                read -p "Use Roaming is set to \"yes\". Change to \"nos\" (strongly recommended)?" yn
+                case $yn in
+                        [Yy]* ) exit ;;
+                        [Nn]* ) sed -i 's/HashKnownHosts no/HashKnownHosts yes/g' $SECURE_SSH_CONFIG ;;
+                        * ) echo "Please answer y (yes) or n (no).";;
+                esac
+        done
+else echo "Use Roaming is set to \"no\". This is the recommended setting."
+
+fi
 
 # Remove prime numbers with size in bits < 2000
 
@@ -68,6 +122,17 @@ else
 	mv $SAFE_MODULI $MODULI
 	rm $ALL_MODULI
 fi
+
+# Display changes
+
+while true; do
+    read -p "Do you want to view your selected changes to ssh_config before committing to those changes?" yn
+    case $yn in
+        [Yy]* ) cat $SECURE_SSH_CONFIG; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer y (yes) or n (no).";;
+    esac
+done
 
 # Prompt user to save changes
 
