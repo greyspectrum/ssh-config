639d29a00167c6bbc856bc3539309b959f089331
greyspectrum <greyspectrum@users.noreply.github.com>
2016-07-25 22:54:30 +0000

    Update ssh-config.sh

--- ssh-config.sh
+++ ssh-config.sh
@@ -11,7 +11,7 @@
 #
 # :author: greyspectrum
 # :date: 20 July 2016
-# :version: 0.0.4
+# :version: 0.9.4
 ##############################################################################
 
 # Define variables (edit these if you want to test this script without altering your ssh_config)
